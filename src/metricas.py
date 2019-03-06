517ce239912ce30493cb0f20e73962f57578fb14
Jessica Temporal <jessicatemporal@gmail.com>
2019-03-06 01:03:11 +0000

    adds source code

---
+++ src/metricas.py
@@ -0,0 +1,54 @@
+def optimal_number_of_clusters(wcss):
+    """
+    Calcula a maior distância entre os pontos que marcam as 
+    somas dos quadrados intra-clusters para 19 calculadas 
+    com `calculate_wcss()`
+    
+    Parametros
+    ----------
+    wcss : lista
+        lista contendo os valores de soma de quadrados intra-cluster
+
+    
+    Returns
+    -------
+    int : número de clusters 
+    """
+    from math import sqrt
+    x1, y1 = 2, wcss[0]
+    x2, y2 = 20, wcss[len(wcss)-1]
+
+    distances = []
+    for i in range(len(wcss)):
+        x0 = i+2
+        y0 = wcss[i]
+
+        numerator = abs((y2-y1)*x0 - (x2-y2)*y0 + x2*y1 - y2*x1)
+        denominator = sqrt((y2 - y1)**2 + (x2 - x1)**2)
+        distances.append(numerator/denominator)
+    return distances.index(max(distances)) + 2
+
+
+def calculate_wcss(data):
+    """
+    Calcula a soma dos quadrados intra-clusters para 19
+    quantidades de clusters, iniciando com o mínimo de 2 clusters
+    
+    Parametros
+    ----------
+    data : DataFrame
+        conjunto de dados para fazer o `.fit()` do KMeans
+
+    
+    Returns
+    -------
+    wcss : lista contendo os valores de soma de quadrados intra-cluster
+    """
+    from sklearn.cluster import KMeans
+    wcss = []
+    for n in range(2, 21):
+        kmeans = KMeans(n_clusters=n)
+        kmeans.fit(X=data)
+        wcss.append(kmeans.inertia_)
+
+    return wcss
