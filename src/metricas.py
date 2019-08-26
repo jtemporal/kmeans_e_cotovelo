c26384c032c1571c6691b8c4c8093d94f21698af
Flavio Clesio <flavioclesio@yahoo.com>
2019-08-26 14:21:17 +0200

    Include min max number of clusters as parameter.
    If the function receives an array with more than 20 positions we'll see only the biggest number of clusters (e.g. K=30 will always return the 30 as the final number of clusters).

--- src/metricas.py
+++ src/metricas.py
@@ -1,4 +1,7 @@
-def optimal_number_of_clusters(wcss):
+min_cluster_range = 2
+max_cluster_range = 20
+
+def optimal_number_of_clusters(wcss, min_cluster_range, max_cluster_range):
     """
     Calcula a maior distância entre os pontos que marcam as 
     somas dos quadrados intra-clusters para 19 calculadas 
@@ -8,15 +11,18 @@ def optimal_number_of_clusters(wcss):
     ----------
     wcss : lista
         lista contendo os valores de soma de quadrados intra-cluster
-
+    min_cluster_range : int
+        numero minimo de clusters
+    max_cluster_range : int
+        numero maximo de clusters
     
     Returns
     -------
     int : número de clusters 
     """
     from math import sqrt
-    x1, y1 = 2, wcss[0]
-    x2, y2 = 20, wcss[len(wcss)-1]
+    x1, y1 = min_cluster_range, wcss[0]
+    x2, y2 = max_cluster_range, wcss[len(wcss)-1]
 
     distances = []
     for i in range(len(wcss)):
@@ -29,7 +35,7 @@ def optimal_number_of_clusters(wcss):
     return distances.index(max(distances)) + 2
 
 
-def calculate_wcss(data):
+def calculate_wcss(data, min_cluster_range, max_cluster_range):
     """
     Calcula a soma dos quadrados intra-clusters para 19
     quantidades de clusters, iniciando com o mínimo de 2 clusters
@@ -38,6 +44,10 @@ def calculate_wcss(data):
     ----------
     data : DataFrame
         conjunto de dados para fazer o `.fit()` do KMeans
+    min_cluster_range : int
+        numero minimo de clusters
+    max_cluster_range : int
+        numero maximo de clusters
 
     
     Returns
@@ -46,7 +56,7 @@ def calculate_wcss(data):
     """
     from sklearn.cluster import KMeans
     wcss = []
-    for n in range(2, 21):
+    for n in range(min_cluster_range, max_cluster_range):
         kmeans = KMeans(n_clusters=n)
         kmeans.fit(X=data)
         wcss.append(kmeans.inertia_)
